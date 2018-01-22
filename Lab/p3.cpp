21291505fe0e13c4d8ed961f6a383d3e8dde9c0b
Yaco <yacosant@ucm.es>
2018-01-22 21:41:51 +0100

    Laboratorio 3 p3 nuevo WA

---
+++ Lab/p3.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+// { 0 < n <= long(v) }
+// fun miradores ( int v[], int n) //return suma; 
+// { suma = : k < i < n : v[i]< max }
+
+int miradores(int v[], int n) {
+	int suma = 0, max=0, i=0;
+	//I = {0<i<=n}
+	//cota= n	
+	while (i < n){ 
+		if (max <= v[i]){
+			max = v[i];
+			suma = max;
+		}
+		else suma += v[i];
+
+		i++;
+	}
+	return suma;
+}
+
+
+bool resuelve() {
+	// Leer tamaño del vector
+	int n, r;
+	cin >> n;
+	if (n == 0)
+		return false;
+
+	// Leer elementos del vector
+	int v[100000];
+	for (int i = 0; i<n; ++i)
+		cin >> v[i];
+
+	r = miradores(v, n);
+	// escribir resultado
+	cout << r << endl;
+	return true;
+}
+
+int main() {
+
+	while (resuelve());
+
+	return 0;
+}
