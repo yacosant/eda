1ed8d2fa1331c550a2ec2ff48ba15b9c78fa8221
Yaco <yacosant@ucm.es>
2018-05-12 15:17:35 +0200

    ejericio 30 hoja 8 pila

---
+++ Ejercicios/8Pila-Ej30.h
@@ -0,0 +1,219 @@
+/**
+[Examen 2º parcial, sept 2017]Extiende el TAD Cola implementado con lista
+enlazada simple visto en clase con una nueva operación cuya cabecera en C++
+es
+
+void llevarAlPrincipio(unsigned int pos);
+
+que lleva el elemento que está en la posición pos de la cola a la primera posición
+de la misma. Ten en cuenta que pos = 1 corresponde al primer elemento de la
+cola; pos = 2 corresponde al segundo; y así sucesivamente.
+Si la posición pos no existe, deberá señalarse un error _Posicion inexistente_.
+Aparte de implementar esta operación, debes indicar la complejidad de la
+misma.
+Para resolver este ejercicio no se puede crear ni destruir memoria dinámica ni
+tampoco modificar los valores almacenados en la cola.
+*/
+#ifndef __COLA_LISTA_ENLAZADA_H
+#define __COLA_LISTA_ENLAZADA_H
+#include <cstddef>
+
+/// Excepciones generadas por algunos m�todos
+class EColaVacia {};
+
+//Excepcion IMPLEMENTACION EXAMEN
+class EPosicionInexistente {};
+
+/**
+ Implementaci�n del TAD Cola utilizando una lista enlazada.
+
+ Las operaciones son:
+
+ - ColaVacia: -> Cola. Generadora implementada en el
+   constructor sin par�metros.
+ - pon: Cola, Elem -> Cola. Generadora
+ - quita: Cola - -> Cola. Modificadora parcial.
+ - primero: Cola - -> Elem. Observadora parcial.
+ - esVacia: Cola -> Bool. Observadora.
+
+ @author Marco Antonio G�mez Mart�n   Mercedes G�mez Albarr�n
+ */
+template <class T>
+class Cola {
+public:
+
+	/** Constructor; operacion ColaVacia */
+	Cola() : _prim(NULL), _ult(NULL) {
+	}
+
+    //IMPLEMENTACION SOLUCION EXAMEN.
+    //COMPLEJIDAD: O(pos);
+
+    void llevarAlPrincipio(unsigned int pos){
+        if(esVacia()) throw EColaVacia();
+        int cont=0;
+        Nodo *ant;
+        Nodo *act;
+
+        while(cont <pos){
+            if(act==NULL) throw EPosicionInexistente();
+            ant= act;
+            act= act->_sig;
+            cont++;
+        }
+
+        ant->_sig = act->_sig;
+        act->_sig = _prim;
+        _prim = act;
+    }
+	
+	/**
+	 A�ade un elemento en la parte trasera de la cola.
+	 Operaci�n generadora.
+
+	 @param elem Elemento a a�adir.
+	*/
+	void pon(const T &elem) {
+		Nodo *nuevo = new Nodo(elem);
+		if (esVacia()) _prim = nuevo; 
+		else _ult->_sig = nuevo;
+		_ult = nuevo;
+	}
+
+	/**
+	 Elimina el primer elemento de la cola.
+	 Operaci�n modificadora parcial.
+
+	 error: falla si la cola est� vac�a
+	*/
+	void quita() {
+		if (esVacia())
+			throw EColaVacia();
+		Nodo *aBorrar = _prim;
+		if (_prim == _ult) 
+			_prim = _ult = NULL;
+		else
+			_prim = _prim->_sig;
+		delete aBorrar;
+	}
+
+	/**
+	 Devuelve el primer elemento de la cola. Operaci�n
+	 observadora parcial.
+	 
+	 error: falla si la cola est� vac�a
+	 @return El primer elemento de la cola.
+	 */
+	const T &primero() const {
+		if (esVacia())
+			throw EColaVacia();
+		return _prim->_elem;
+	}
+
+	/**
+	 Indica si la cola tiene elementos o no.
+
+	 @return true si la cola no tiene ning�n elemento.
+	 */
+	bool esVacia() const {
+		return _prim == NULL;
+	}
+
+
+	/** Destructor; elimina la lista enlazada. */
+	~Cola() {
+		libera();
+		_prim = _ult = NULL;
+	}
+
+
+	/** Constructor copia */
+	Cola(const Cola<T> &other) : _prim(NULL), _ult(NULL) {
+		copia(other);
+	}
+
+	/** Operador de asignaci�n */
+	Cola<T> &operator=(const Cola<T> &other) {
+		if (this != &other) { 
+			libera();
+			copia(other);
+		}
+		return *this;
+	}
+
+	/** Operador de comparaci�n. */
+	bool operator==(const Cola<T> &rhs) const {
+		Nodo *p1 = _prim;
+		Nodo *p2 = rhs._prim;
+		while ((p1 != NULL) && (p2 != NULL) && (p1->_elem == p2->_elem)) {
+			p1 = p1->_sig;
+			p2 = p2->_sig;
+		}
+		return (p1 == NULL) && (p2 == NULL);
+	}
+
+	bool operator!=(const Cola<T> &rhs) const {
+		return !(*this == rhs);
+	}
+	
+
+private:
+
+	/**
+	 Clase nodo que almacena internamente el elemento (de tipo T),
+	 y un puntero al nodo siguiente, que podr�a ser NULL si
+	 el nodo es el �ltimo de la lista enlazada.
+	 */
+	class Nodo {
+	public:
+		Nodo() : _sig(NULL) {}
+		Nodo(const T &elem) : _elem(elem), _sig(NULL) {}
+		Nodo(const T &elem, Nodo *sig) : 
+		    _elem(elem), _sig(sig) {}
+
+		T _elem;
+		Nodo *_sig;
+	};
+
+
+	/**
+	Genera una copia de other en la cola receptora
+	*/
+	void copia(const Cola &other) {
+		if (other.esVacia())
+			_prim = _ult = NULL;
+		else {
+			Nodo *puntAOrigen = other._prim; 
+			Nodo *ultimo; 
+			_prim = new Nodo(puntAOrigen->_elem); 
+			ultimo = _prim; 
+			while (puntAOrigen->_sig != NULL) { 
+				puntAOrigen = puntAOrigen->_sig; 
+				ultimo->_sig = new Nodo(puntAOrigen->_elem); 
+				ultimo = ultimo->_sig; 
+			}
+			_ult = ultimo;
+		}
+	}
+
+	
+	/**
+	 Elimina todos los nodos de la lista enlazada que soporta la cola.
+	 */
+	void libera() {
+		while (_prim != NULL) {
+			Nodo *aBorrar = _prim;
+			_prim = _prim->_sig;
+			delete aBorrar;
+		}
+	}
+
+	
+	/** Puntero al primer elemento. */
+	Nodo *_prim;
+
+	/** Puntero al �ltimo elemento. */
+	Nodo *_ult;
+};
+
+#endif // __PILA_LISTA_ENLAZADA_H
