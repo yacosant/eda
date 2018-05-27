309f06bb3de8e7c7bce77ed04dee9228372c44d3
Yaco <yacosant@ucm.es>
2018-05-27 20:16:36 +0200

    T4Recursivo-Ej4

---
+++ Ejercicios/4Recursivo-Ej4.cpp
@@ -0,0 +1,27 @@
+﻿/*
+T04 - Ej4
+
+4. Implementa una función recursiva simple cuadrado que calcule el cuadrado de un
+número natural n, basándote en el siguiente análisis de casos:
+Caso directo: Si n = 0, entonces n2 = 0
+Caso recursivo: Si n > 0, entonces n2 = (n − 1)^2 + 2*(n − 1) + 1
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int potencia(int n) {
+	if (n == 0) return 0;
+	else return potencia(n - 1) + 2 * (n - 1) + 1;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	while (n > 0) {
+		cout << "El cuadrado de " << n << " es: " << potencia(n) << endl;
+		cin >> n;
+	}
+	return 0;
+}
