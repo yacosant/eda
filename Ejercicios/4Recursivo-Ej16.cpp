1420f63f54a2966cd6e2681a53b4b791c3caf6d3
Yaco <yacosant@ucm.es>
2018-05-27 20:16:49 +0200

    T4Recursivo-Ej16

---
+++ Ejercicios/4Recursivo-Ej16.cpp
@@ -0,0 +1,31 @@
+﻿/*
+T04 - Ej16
+
+16. Especifica e implementa un algoritmo recursivo que dado un vector v de enteros, que
+viene dado en orden estrictamente creciente, determine si el vector contiene alguna
+posición i que cumpla v[i] = i. Utilizando el método de despliegue de recurrencias,
+*/
+
+#include <iostream>
+using namespace std;
+
+bool coincidencia(int v[],int n, int i) {
+	if (n == i) return false;
+	else if (v[i] == i) return true;
+	else return coincidencia(v, n, i+1);
+}
+
+int main() {
+	int n, i=0;
+	int v[10];
+
+	cin >> n;
+	while (i<10 && n>=0) {
+		v[i] = n;
+		cin >> n;
+		i++;
+	}
+	cout << coincidencia(v, i, 0);
+	system("pause");
+	return 0;
+}
